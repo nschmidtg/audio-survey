90e26346619b49b13fc2b49ad247c0ee61a228f4
Nicolas Schmidt <nschmidtg@gmail.com>
2020-11-10 15:38:29 +0100

    javascript directory change for compilation/validations

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,8 @@ class Song < ApplicationRecord
 
     enum rating_strings: [ 'strong'.to_sym, 'somewhat'.to_sym, 'no association'.to_sym ]
 
+    validates :recognized, presence: true
+    validates :rating, presence: true
     validates :recognize_text, presence: true, if: :recognized?
     validates :memory_description, presence: true, if: -> { rating == 0 || rating == 1 }
 end
