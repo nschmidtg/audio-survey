b3e47dbcae1fdc3ac5e979dee889c3d3285b2000
Marina Nieto <marinanietogimenez@gmail.com>
2020-11-21 15:41:34 +0100

    Attempt to fix locale - 5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
   private
   def set_locale
-    locale = params[:locale].to_s.strip.to_sym
+    locale = extract_locale
     I18n.locale = I18n.available_locales.include?(locale) ?
         locale :
         I18n.default_locale
