9008c6593bac4babc5ff964e8d8b9d4834bbe0e1
Marina Nieto <marinanietogimenez@gmail.com>
2020-11-21 19:36:15 +0100

    Goof2

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Survey
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.i18n.avilable_locales = [:en, :es]
+    config.i18n.available_locales = [:en, :es]
     config.i18n.default_locale = :en
   end
 end
