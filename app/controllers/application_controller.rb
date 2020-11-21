e1406c17babcc867660e9c7705f649b8dc97b23e
Marina Nieto <marinanietogimenez@gmail.com>
2020-11-21 19:32:37 +0100

    Goof

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     I18n.locale = params[:lang] || locale_from_header || I18n.default_locale
-
+  end
   def locale_from_header
     request.env.fetch('HTTP_ACCEPT_LANGUAGE', '').scan(/[a-z]{2}/).first
   end
