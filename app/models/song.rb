00d558345c86ddd958d813a800d8f6eb88721619
Marina Nieto <marinanietogimenez@gmail.com>
2020-11-22 14:25:04 +0100

    Small fix & small tweaks

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,6 @@
 class Song < ApplicationRecord
     belongs_to :user
 
-    enum rating_strings: [ I18n.t(:yes_label).to_sym, I18n.t(:somewhat_label).to_sym, I18n.t(:no_label).to_sym ]
-
     validates :recognized, inclusion: { in: [ true, false ] }
     validates :rating, presence: true
     validates :recognize_text, presence: true, if: :recognized?
