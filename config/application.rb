000236d201db64a10881245ce7cb81b954da145d
Marina Nieto <marinanietogimenez@gmail.com>
2020-11-22 01:36:19 +0100

    Attempt to fix fallbacks - 4

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,7 @@ module Survey
     # the framework and any gems in your application.
 
     config.i18n.available_locales = [:en, :es, :da, :ca, :eu]
-    config.i18n.fallbacks.map(:ca => :es)
-    config.i18n.default_locale = :en
+    # config.i18n.default_locale = :en
     config.i18n.fallbacks = {:da => [:en], :ca => [:es], :eu => [:es]}
     #config.i18n.fallbacks = true
   end
