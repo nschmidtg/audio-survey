c386208b2bbd1b05de951f2316d3c88d44e37964
Nicolas Schmidt <nschmidtg@gmail.com>
2020-11-23 22:24:35 +0100

    fix/song-id: disable back button browser

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -14,10 +14,13 @@ class SongController < ApplicationController
   def edit
   end
 
+  def show
+    redirect_to edit_song_path(id: params[:id])
+  end
+
   def update
     if @song.update_attributes(params_song)
       @song.update_attribute(:completed, true)
-      flash[:notice] = 'Imagen acutalizada'
       next_song = @song.user.songs.where(completed: false).first
       if next_song
         redirect_to edit_song_path(next_song.id)
@@ -32,13 +35,13 @@ class SongController < ApplicationController
   private
 
   def find_song
-    @song = @current_user.songs.where(id: params[:id]).first
+    @song = @current_user.songs.where(id: params[:id], completed: false).first
     if @song
       @total = @song.user.songs.count
       @completed = @song.user.songs.where(completed: true).count
     else
       flash[:error] = t(:error)
-      redirect_to root_path
+      redirect_to "/users?token=#{@current_user.token}"
     end
   end
 
@@ -55,7 +58,7 @@ class SongController < ApplicationController
   def authenticate
     @current_user = User.find_by(token: session[:current_user_token])
     unless @current_user
-      flash[:error] = "Acción no permitida"
+      flash[:error] = t(:error)
       redirect_to root_path
     end
   end
